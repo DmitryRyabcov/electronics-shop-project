4c18977960aa752a1c1f2a16166c55c8bbb9b058
DmitryRyabcov <ryacov_dima@mail.ru>
2023-11-13 15:27:10 +0300

    Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+@pytest.fixture
+def test_class():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_func(test_class):
+    assert test_class.name == "Смартфон"
+    assert test_class.price == 10000
+    assert test_class.quantity == 20
+
