c6be705f984e6f4994a2fabe68e3ccbf7f64d539
DmitryRyabcov <ryacov_dima@mail.ru>
2023-11-18 13:42:31 +0300

    Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -11,3 +11,9 @@ def test_func(test_class):
     assert test_class.price == 10000
     assert test_class.quantity == 20
 
+def test_price():
+    assert 10000 * Item.pay_rate == 8000.0
+
+def test_total_price(test_class):
+    assert test_class.price * test_class.quantity == 200000
+
